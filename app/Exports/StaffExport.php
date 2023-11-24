67f1af674951fdd710730f05528ea722a14b9712
張育誠 <johnny.chang@js-adways.com.tw>
2023-11-24 16:51:21 +0800

    chore:excel download phoneEncrypt 改到StaffExport處理->資料庫撈出資料後直接處理

--- app/Exports/StaffExport.php
+++ app/Exports/StaffExport.php
@@ -18,7 +18,12 @@ class StaffExport implements WithMultipleSheets
     {
         $sheets = [];
 
-        $staffCollection = Staff::whereIn('id', $this->staff)->get();
+        $staffCollection = Staff::whereIn('id', $this->staff)->get()->map(function ($staff) {
+            $replace = ['(', ')', '+', '-'];
+            $staff->phone = str_replace($replace, '', $staff->phone);
+            $staff->phone = substr($staff->phone, 0, 3) . '###' . substr($staff->phone, -4);
+            return $staff;
+        });
 
         $sheets = $staffCollection->chunk(10)->map(function ($chunk) {
             return new StaffSheet($chunk);
--- app/Exports/StaffSheet.php
+++ app/Exports/StaffSheet.php
@@ -14,18 +14,8 @@ class StaffSheet implements FromView, ShouldAutoSize, WithTitle
     public function __construct($staff)
     {
         $this->staff = $staff;
-        $this->phoneEncrypt();
     }
 
-    protected function phoneEncrypt()
-    {
-        $this->staff = $this->staff->map(function ($staff) {
-            $replace = ['(', ')', '+', '-'];
-            $staff->phone = str_replace($replace, '', $staff->phone);
-            $staff->phone = substr($staff->phone, 0, 3) . '###' . substr($staff->phone, -4);
-            return $staff;
-        });
-    }
 
     public function view(): View
     {
