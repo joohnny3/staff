0f78333b7de41ebb3c5f7aaf3cf78b417a11823d
張育誠 <johnny.chang@js-adways.com.tw>
2024-03-11 15:31:21 +0800

    style:resign.blade 樣式更改

--- resources/views/emails/resign.blade.php
+++ resources/views/emails/resign.blade.php
@@ -5,16 +5,16 @@
     <title>Employee Resignation List</title>
     <style>
         body {
-            font-family: 'Microsoft JhengHei', sans-serif;
+            font-family: '微軟正黑體', sans-serif;
             font-size: 10pt;
         }
         table {
-            width: 35%;
+            width: 551px;
             border-collapse: collapse;
         }
         th, td {
             border: 0.5pt solid black;
-            padding: 5px;
+            padding: 2.78px;
             text-align: center;
         }
         th {
@@ -33,35 +33,41 @@
         .sign {
             margin-top: 20px;
         }
+        .employee-id { width: 55px; }
+        .name { width: 51px; }
+        .name-en { width: 65px; }
+        .department { width: 115px; }
+        .resignation-date, .last-working-day { width: 85px; }
+        .note { width: 95px; }
     </style>
 </head>
 <body>
 <p>Dear all,</p>
-<p>近期離職名單如下，共計 {{ count($content->resignations) }} 位，請留意最後工作日，並協助當日離退移交流程，謝謝！</p>
+<p>近期離職名單如下，共計 {{ count($content->resignations) }} 位，請留意<b style="font-weight: bold; color: #0000FF;">最後工作日</b>，並協助當日離退移交流程，謝謝！</p>
 
 <table>
     <thead>
     <tr>
-        <th>員編</th>
-        <th>姓名</th>
-        <th>英文姓名</th>
-        <th>部門</th>
-        <th class="leave-date">離職日</th>
+        <th class="employee-id">員編</th>
+        <th class="name">姓名</th>
+        <th class="name-en">英文姓名</th>
+        <th class="department">部門</th>
+        <th class="resignation-date leave-date">離職日</th>
         <th class="last-working-day">最後工作日</th>
-        <th>備註</th>
+        <th class="note">備註</th>
     </tr>
     </thead>
     <tbody>
     @foreach ($content->resignations as $resignation)
         <tr>
-            <td>{{ $resignation->employee_id }}</td>
-            <td>{{ $resignation->name }}</td>
-            <td>{{ $resignation->name_en }}</td>
-            <td>{{ $resignation->department }}</td>
-            <td class="leave-date">{{ $resignation->resignation_date }}</td>
+            <td class="employee-id">{{ $resignation->employee_id }}</td>
+            <td class="name">{{ $resignation->name }}</td>
+            <td class="name-en">{{ $resignation->name_en }}</td>
+            <td class="department">{{ $resignation->department }}</td>
+            <td class="resignation-date leave-date">{{ $resignation->resignation_date }}</td>
             <td class="{{ $resignation->note ? 'last-working-day note-present' : 'last-working-day' }}">
                 {{ $resignation->last_working_day }}</td>
-            <td>{{ $resignation->note }}</td>
+            <td class="note">{{ $resignation->note }}</td>
         </tr>
     @endforeach
     </tbody>
